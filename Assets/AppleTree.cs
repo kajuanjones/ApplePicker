5affd69748e4919b0f0c4e470ef34b98e546df52
KaJuan Jones <47114033+kajuanjones@users.noreply.github.com>
2019-09-05 12:34:43 -0400

    Project Start and First Scripts
    Started the project. Made a AppleTree prefab and started the AppleTree script.

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+    }
+}
